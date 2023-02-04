3c68b8f4af846ae38881cc2eec89006ed60c41ee
Artem Kalach <rtmklcwork@gmail.com>
2023-02-04 07:52:32 +0300

    commit for checking savedRequests2

--- src/components/SavedRequests.jsx
+++ src/components/SavedRequests.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 
 const SavedRequests = () => {
-    const requests = useSelector(state=>state.requests.requests)
+  const requests = useSelector(state => state.requests.requests)
   return (
     <div>
-{    console.log(requests)
-}
-      {requests.map((item)=>{
-        <div>{item}</div>
+      {console.log(requests)
+      }
+      {requests.map((item) => {
+        return (
+          <div>
+            <button>{item}</button>
+          </div>
+
+        )
       })}
     </div>
   )
